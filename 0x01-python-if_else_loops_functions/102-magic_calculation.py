a4bd1362536a5477ccdfe40f3bdc839f25d09cdf
xGamal <xgamaleldin@gmail.com>
2023-11-28 16:18:25 -0800

    fix101

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Abdelrahman Gamaleldin
+
+def magic_calculation(a, b, c):
+    """by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
